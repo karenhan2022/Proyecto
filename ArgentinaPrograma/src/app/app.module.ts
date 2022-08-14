70b7b46d57ee771f06086b06a0d65339b76e5423
karenhan2022 <karenhan2022@gmail.com>
2022-08-14 06:36:46 -0300

    Creando banner y about
    Creando nuevamente banner luego de errores y agregado about

--- ArgentinaPrograma/src/app/app.module.ts
+++ ArgentinaPrograma/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoArgentinaProgramaComponent } from './components/logo-argentina-programa/logo-argentina-programa.component';
 import { RedessocialesComponent } from './components/redessociales/redessociales.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoArgentinaProgramaComponent,
-    RedessocialesComponent
+    RedessocialesComponent,
+    BannerComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
